6fbea259a504644440c75053b1b30280c0792a8f
Ilke Zilci <ilke.zilci@tu-berlin.de>
2015-10-23 16:18:30 +0200

    wp-cli correctly configured in docker image

--- components/wordpress/wp-setup.sh
+++ components/wordpress/wp-setup.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 #install wordpress command line tool
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#chmod +x wp-cli.phar
+#sudo mv wp-cli.phar /usr/local/bin/wp
 
 #configure wp-cli
 #inside the current working directory: wp-config.yml 
 
+wp core install --path=/var/www/html --title=CYCLONE --admin_user=cycloneadmin --admin_password=mllzsnXMAZWioX --url=http://localhost --admin_email=ilke.zilci@tu-berlin.de --allow-root
+
 #install and activate generic-openid-connect plugin
-wp plugin install generic-openid-connect.1.0.zip --activate
+wp plugin install generic-openid-connect.1.0.zip --activate --allow-root
 
 #configure generic-openid-connect plugin
-wp option add gen_openid_con_client_id test                                                                                      
+wp option add gen_openid_con_client_id test --allow-root                                                                                     
 #wp option add gen_openid_con_client_secret 	                                                                                           
-wp option add gen_openid_con_ep_login https://federation.cyclone-project.eu/auth/realms/master/protocol/openid-connect/auth     
-wp option add gen_openid_con_ep_token https://federation.cyclone-project.eu/auth/realms/master/protocol/openid-connect/token    
-wp option add gen_openid_con_ep_userinfo https://federation.cyclone-project.eu/auth/realms/master/protocol/openid-connect/userinfo 
-wp option add gen_openid_con_identity_key sub                                                                                       
-wp option add gen_openid_con_no_sslverify 1
-wp option add gen_openid_con_scope openid                                                                                    
-wp option add gen_openid_con_use_autologin 1
-wp option add gen_openid_con_allowed_regex '([A-Za-z0-9\-\_]+)'
+wp option add gen_openid_con_ep_login https://federation.cyclone-project.eu/auth/realms/master/protocol/openid-connect/auth --allow-root   
+wp option add gen_openid_con_ep_token https://federation.cyclone-project.eu/auth/realms/master/protocol/openid-connect/token --allow-root   
+wp option add gen_openid_con_ep_userinfo https://federation.cyclone-project.eu/auth/realms/master/protocol/openid-connect/userinfo --allow-root
+wp option add gen_openid_con_identity_key sub --allow-root                                                                                      
+wp option add gen_openid_con_no_sslverify 1 --allow-root
+wp option add gen_openid_con_scope openid --allow-root                                                                                   
+wp option add gen_openid_con_use_autologin 1 --allow-root
+wp option add gen_openid_con_allowed_regex '([A-Za-z0-9\-\_]+)' --allow-root
 
 
