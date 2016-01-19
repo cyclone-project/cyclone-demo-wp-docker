bf7ef7d91be047bcdce7595576fd00938164fbab
Ilke Zilci <ilke.zilci@tu-berlin.de>
2016-01-19 16:24:23 +0100

    fixed wp-config.php

--- components/wordpress/wp-config.php
+++ components/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'tastamam');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '2d16f9f40e290e6e02bc03aaf2d32a1b06f80aad');
+define('SECURE_AUTH_KEY',  'a5918af5387f2834ef2da78c590f98ca221240bd');
+define('LOGGED_IN_KEY',    '3c7c54e6e74b948ecafdaa82a6179a1904940451');
+define('NONCE_KEY',        '7dec586030ff4cdbf13b6e9698793473e8f06414');
+define('AUTH_SALT',        '0b1d51b95189fda06782a6453183366aba69567f');
+define('SECURE_AUTH_SALT', '85d8550ef6ce42faa4664ab642957d183052da12');
+define('LOGGED_IN_SALT',   '1b432cda147ff814a41efcd975fe1e5bcdd543b5');
+define('NONCE_SALT',       '710f1f607c3a4e8744dbef6a5994cca6f9a5d727');
 
 /**#@-*/
 
@@ -77,8 +77,13 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
