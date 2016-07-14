d435ee6e6e021f61d2ba6a6a2a44bdbee242b3b6
Ilke Zilci <ilke.zilci@tu-berlin.de>
2016-07-14 12:41:21 +0200

    pass in vm url from nuvla

--- wpdemo_SSRecipe_OpenStack.sh
+++ wpdemo_SSRecipe_OpenStack.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -x
-sudo apt-get -y update 
-sudo apt-get -y install curl
 
 curl -sSL https://get.docker.com/ | sh
 #docker installs git, but just in case attempt to install it again here
-sudo apt-get install -y git
+apt-get install -y git
 
 #get the preconfigured keycloak Dockerfile
-sudo mkdir /opt/wpRepo
+mkdir /opt/wpRepo
 cd /opt/wpRepo
-sudo git clone https://github.com/cyclone-project/cyclone-demo-wp-docker.git
+git clone https://github.com/cyclone-project/cyclone-demo-wp-docker.git
 cd cyclone-demo-wp-docker/
 #start docker as daemon
 #service docker start
@@ -18,10 +16,17 @@ cd cyclone-demo-wp-docker/
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose" 
+curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+#port=$(ss-get port)
+#url="http://$(ss-get hostname):$port"
+siteurl="http://$(ss-get hostname)"
+
+echo $siteurl | tee components/wordpress/url.txt
+
 
 # build and deploy with docker-compose
-sudo docker-compose build
-sudo docker-compose up
+docker-compose build
+docker-compose up -d
+
